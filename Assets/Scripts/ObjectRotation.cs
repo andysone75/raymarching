007fc2b03dcb9c94d4cd80f2e089b2ba1b427d2c
Andrey Kolyada <andrey.kolyada.00@yandex.ru>
2020-02-08 23:32:33 +0900

    End of the day | 08.02.2020

---
+++ Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    private Vector3 localEulerAngles;
+    [Range(0,100)]
+    public float speed = 0f;
+
+    private void Start()
+    {
+        localEulerAngles = transform.localEulerAngles;
+    }
+
+    void Update()
+    {
+        localEulerAngles.y += speed * Time.deltaTime;
+        transform.localEulerAngles = localEulerAngles;
+    }
+}
